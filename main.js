4c91026d0d7ab679d249a68d7f5b8b22268f2b84
Ekta Srivastava <146434334+ekta-developer@users.noreply.github.com>
2024-12-03 16:47:46 +0530

    Practise JS
    Practise JS

---
+++ main.js
@@ -0,0 +1,5 @@
+// const myHeading = document.querySelector("h1");
+// myHeading.textContent = "Hello World!";
+
+const myHeading=document.getElementById("heading");
+myHeading.textContent="Hello world !"
